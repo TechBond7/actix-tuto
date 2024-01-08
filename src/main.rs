ff319b128cfaa131df9fea4ba8b99cce1889d54e
TechBond7 <techbond7@outlook.com>
2024-01-08 14:43:53 +0200

    state extractor using atomic

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
-use actix_web::{web, App, HttpServer, Responder};
-use std::cell::Cell;
+use actix_web::{get, web, App, HttpServer, Responder};
+use std::{
+    cell::Cell,
+    sync::atomic::{AtomicUsize, Ordering},
+    sync::Arc,
+};
 
 #[derive(Clone)]
 struct AppState {
-    count: Cell<usize>,
+    local_count: Cell<usize>,
+    global_count: Arc<AtomicUsize>,
 }
 
+#[get("/")]
 async fn show_count(data: web::Data<AppState>) -> impl Responder {
-    format!("count: {}", data.count.get())
+    format!(
+        "global_count: {}\nlocal_count: {}",
+        data.global_count.load(Ordering::Relaxed),
+        data.local_count.get()
+    )
 }
 
+#[get("/add")]
 async fn add_one(data: web::Data<AppState>) -> impl Responder {
-    let count = data.count.get();
-    data.count.set(count + 1);
+    data.global_count.fetch_add(1, Ordering::Relaxed);
 
-    format!("count: {}", data.count.get())
+    let local_count = data.local_count.get();
+    data.local_count.set(local_count + 1);
+
+    format!(
+        "global_count: {}\nlocal_count: {}",
+        data.global_count.load(Ordering::Relaxed),
+        data.local_count.get()
+    )
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let data = AppState {
-        count: Cell::new(0),
+        local_count: Cell::new(0),
+        global_count: Arc::new(AtomicUsize::new(0)),
     };
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(data.clone()))
-            .route("/", web::to(show_count))
-            .route("/add", web::to(add_one))
+            .service(show_count)
+            .service(add_one)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
