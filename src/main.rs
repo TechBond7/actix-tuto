bafeef7d8194f5fd127190ec852bd6b385e7ed0f
TechBond7 <techbond7@outlook.com>
2024-01-08 20:54:58 +0200

    generating resource urls

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use actix_web::{get, web, App, HttpServer, Result};
-use serde::Deserialize;
+use actix_web::{get, guard, http::header, HttpRequest, HttpResponse, Result};
 
-#[derive(Deserialize)]
-struct Info {
-    username: String,
-}
+#[get("/test")]
+async fn index(req: HttpRequest) -> Result<HttpResponse> {
+    let url = req.url_for("foo", ["1", "2", "3"])?;
 
-#[get("/{username}/index.html")]
-async fn index(info: web::Path<Info>) -> Result<String> {
-    Ok(format!("Welcome {}", info.username))
+    Ok(HttpResponse::Found()
+        .insert_header((header::LOCATION, url.as_str()))
+        .finish())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    use actix_web::{web, App, HttpServer};
+
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::resource("/test/{a}/{b}/{c}")
+                    .name("foo")
+                    .guard(guard::Get())
+                    .to(HttpResponse::Ok),
+            )
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
