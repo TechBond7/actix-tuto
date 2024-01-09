5edf0fc9f1d06c7f9d1ab0cb7a0bef33f5a31381
TechBond7 <techbond7@outlook.com>
2024-01-09 19:11:24 +0200

    example of user session

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-use actix_web::{dev::Service as _, web, App};
-use futures_util::future::FutureExt;
+use actix_session::{storage::CookieSessionStore, Session, SessionMiddleware};
+use actix_web::{cookie::Key, web, App, Error, HttpResponse, HttpServer};
+
+async fn index(session: Session) -> Result<HttpResponse, Error> {
+    // access session data
+    if let Some(count) = session.get::<i32>("counter")? {
+        session.insert("counter", count + 1)?;
+    } else {
+        session.insert("counter", 1)?;
+    }
+
+    Ok(HttpResponse::Ok().body(format!(
+        "Count is {:?}!",
+        session.get::<i32>("counter")?.unwrap()
+    )))
+}
 
 #[actix_web::main]
-async fn main() {
-    let app = App::new()
-        .wrap_fn(|req, srv| {
-            println!("Hi from start. You requested: {}", req.path());
-            srv.call(req).map(|res| {
-                println!("Hi from response");
-                res
-            })
-        })
-        .route(
-            "/index.html",
-            web::get().to(|| async { "Hello, middleware!" }),
-        );
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(
+                // create cookie based session middleware
+                SessionMiddleware::builder(CookieSessionStore::default(), Key::from(&[0; 64]))
+                    .cookie_secure(false)
+                    .build(),
+            )
+            .service(web::resource("/").to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
